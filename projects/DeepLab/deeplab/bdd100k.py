65d57abce6ee064f69e1843c7ac4fd1cd2c2c49a
tomo <tomo14142356@gmail.com>
2022-03-16 21:18:05 +0900

    Fix cutom dataset def for bdd100k, add test code

--- projects/DeepLab/deeplab/bdd100k.py
+++ projects/DeepLab/deeplab/bdd100k.py
@@ -63,30 +63,27 @@ def _get_bdd100k_files(image_dir, label_dir):
     # scan through the directory
     videos = PathManager.ls(image_dir)
     logger.info(f"{len(videos)} videos found in '{image_dir}'.")
-    for video in videos:
-        video_img_dir = os.path.join(image_dir, video)
-        video_label_dir = os.path.join(label_dir, video)
-        for basename in PathManager.ls(video_img_dir):
-            image_file = os.path.join(video_img_dir, basename)
+    for basename in videos:
+        image_file = os.path.join(image_dir, basename)
 
-            suffix = ".jpg"
-            assert basename.endswith(suffix), basename
-            basename = basename[: -len(suffix)]
+        suffix = ".jpg"
+        assert basename.endswith(suffix), basename
+        basename = basename[: -len(suffix)]
 
-            label_file = os.path.join(video_label_dir, basename + ".png")
+        label_file = os.path.join(label_dir, basename + ".png")
 
-            files.append((image_file, label_file))
+        files.append((image_file, label_file))
     assert len(files), "No images found in {}".format(image_dir)
     for f in files[0]:
         assert PathManager.isfile(f), f
     return files
 
 
-def load_bdd100k_semantic(image_dir, gt_dir):
+def load_bdd100k_semantic(image_dir, label_dir):
     ret = []
     # gt_dir is small and contain many small files. make sense to fetch to local first
-    gt_dir = PathManager.get_local_path(gt_dir)
-    for image_file, label_file in _get_bdd100k_files(image_dir, gt_dir):
+    label_dir = PathManager.get_local_path(label_dir)
+    for image_file, label_file in _get_bdd100k_files(image_dir, label_dir):
         ret.append(
             {
                 "file_name": image_file,
@@ -128,3 +125,17 @@ def register_bdd100k(root):
 if __name__.endswith(".bdd100k"):
     _root = os.path.expanduser(os.getenv("DETECTRON2_DATASETS", "datasets"))
     register_bdd100k(_root)
+
+if __name__ == "__main__":
+    print("test start")
+    _root = os.path.expanduser(os.getenv("DETECTRON2_DATASETS", "datasets"))
+    register_bdd100k(_root)
+    SPLITS = {
+        "bdd100k_sem_seg_train": (
+            "bdd100k/seg/images/train/",
+            "bdd100k/seg/labels/train/",
+        ),
+        "bdd100k_sem_seg_val": ("bdd100k/seg/images/val/", "bdd100k/seg/labels/val/"),
+    }
+    val_key = list(SPLITS.keys())[0]
+    data = DatasetCatalog.get(val_key)
