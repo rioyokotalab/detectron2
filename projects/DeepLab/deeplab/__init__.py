3521c1e7e54c640b862c804cae417d5ea630a7ed
tomo <tomo14142356@gmail.com>
2022-03-21 19:57:05 +0900

    Add my evaluator for deeplab

--- projects/DeepLab/deeplab/__init__.py
+++ projects/DeepLab/deeplab/__init__.py
@@ -6,3 +6,7 @@ from .semantic_seg import DeepLabV3Head, DeepLabV3PlusHead
 from .augmentation_impl import FixedSizeCrop
 from .bdd100k import load_bdd100k_semantic, register_bdd100k
 from .bdd100k import _get_bdd100k_files, _get_bdd100k_metadata
+from .sem_seg_evaluation import SemSegEvaluator
+from .cityscapes_evaluation import CityscapesEvaluator
+from .cityscapes_evaluation import CityscapesInstanceEvaluator
+from .cityscapes_evaluation import CityscapesSemSegEvaluator
---
+++ projects/DeepLab/deeplab/sem_seg_evaluation.py
@@ -0,0 +1,48 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+import numpy as np
+import os
+import PIL.Image as Image
+
+from detectron2.utils.file_io import PathManager
+from detectron2.evaluation.evaluator import SemSegEvaluator as BaseEvaluator
+
+
+class SemSegEvaluator(BaseEvaluator):
+    """
+    Evaluate semantic segmentation metrics.
+    """
+
+    def reset(self):
+        super().reset()
+        PathManager.mkdirs(self._output_dir)
+
+    def process(self, inputs, outputs):
+        """
+        Args:
+            inputs: the inputs to a model.
+                It is a list of dicts. Each dict corresponds to an image and
+                contains keys like "height", "width", "file_name".
+            outputs: the outputs of a model. It is either list of semantic segmentation predictions
+                (Tensor [H, W]) or list of dicts with key "sem_seg" that contains semantic
+                segmentation prediction in the same format.
+        """
+        for input, output in zip(inputs, outputs):
+            output = output["sem_seg"].argmax(dim=0).to(self._cpu_device)
+            pred = np.array(output, dtype=np.int)
+            input_gt_file = self.input_file_to_gt_file[input["file_name"]]
+            with PathManager.open(input_gt_file, "rb") as f:
+                gt = np.array(Image.open(f), dtype=np.int)
+
+            gt[gt == self._ignore_label] = self._num_classes
+
+            self._conf_matrix += np.bincount(
+                (self._num_classes + 1) * pred.reshape(-1) + gt.reshape(-1),
+                minlength=self._conf_matrix.size,
+            ).reshape(self._conf_matrix.shape)
+
+            self._predictions.extend(self.encode_json_sem_seg(pred, input["file_name"]))
+            file_name = os.path.basename(input["file_name"])
+            file_name = os.path.splitext(file_name)[0]
+            file_path = os.path.join(self._output_dir, f"{file_name}_sem_seg.png")
+            print(file_path, pred.shape, pred.dtype)
+            Image.fromarray(pred.astype(np.uint8)).save(file_path)
