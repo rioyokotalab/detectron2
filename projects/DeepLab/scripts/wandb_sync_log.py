ec517ed4cfb9a73bfc7ceed7cb47795ea7994a7f
tomo <tomo14142356@gmail.com>
2022-07-02 10:51:20 +0900

    Fix wandb name making rule, lint code

--- projects/DeepLab/scripts/wandb_sync_log.py
+++ projects/DeepLab/scripts/wandb_sync_log.py
@@ -10,12 +10,18 @@ import wandb
 def rename_wandb_name_path(path, remove_str):
     wandb_name = path
     wandb_name = wandb_name.replace(f"{remove_str}", "")
+    wandb_name = wandb_name.replace("pretrain", "")
+    wandb_name = wandb_name.replace("multi", "")
+    wandb_name = wandb_name.replace("/resnet50", "")
+    wandb_name = wandb_name.replace("64_2", "")
+    wandb_name = wandb_name.replace("epochs_100", "")
+    wandb_name = wandb_name.replace("resume", "")
     wandb_name = wandb_name.replace("/", "_")
     wandb_name = wandb_name.replace("pixpro_base_r50_100ep", "")
     wandb_name = wandb_name.replace("convert_d2_models", "")
+    wandb_name = wandb_name.replace("__", "_")
     wandb_name = wandb_name.rstrip("_")
     wandb_name = wandb_name.lstrip("_")
-    wandb_name = wandb_name.replace("__", "_")
     return wandb_name
 
 
@@ -23,8 +29,8 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--root_path", default="/home/tomo/ssl_pj/detectron2/projects/DeepLab/output"
-    )
+        "--root_path",
+        default="/home/tomo/ssl_pj/detectron2/projects/DeepLab/output")
     parser.add_argument("--project", default="detectron2")
     parser.add_argument("--entity", default="tomo")
     parser.add_argument("--target_path", default="")
@@ -37,9 +43,10 @@ if __name__ == "__main__":
 
     wandb_name = rename_wandb_name_path(root, root_path)
     wandb_id = args.id
-    run = wandb.init(
-        entity=args.entity, project=args.project, name=wandb_name, id=wandb_id
-    )
+    run = wandb.init(entity=args.entity,
+                     project=args.project,
+                     name=wandb_name,
+                     id=wandb_id)
     wandb_id = run.id
     print("wandb_id:", wandb_id)
     print("wandb_dir:", root)
@@ -48,14 +55,17 @@ if __name__ == "__main__":
 
     file_or_dirs = sorted(glob.glob(f"{root}/**", recursive=True))
 
-    require_files = ["model_final.pth", ".out", ".txt", "metrics.json", "config.yaml"]
+    require_files = [
+        "model_final.pth", ".out", ".txt", "metrics.json", "config.yaml"
+    ]
 
     is_config_save = True
 
     for file_or_dir in file_or_dirs:
         if os.path.isfile(file_or_dir):
             is_require_files = any(w in file_or_dir for w in require_files)
-            is_require_files = is_require_files and not ("events." in file_or_dir)
+            is_require_files = is_require_files and not ("events."
+                                                         in file_or_dir)
             if is_require_files:
                 if args.upload:
                     print("save file:", file_or_dir, file=sys.stderr)
