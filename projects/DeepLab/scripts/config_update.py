cc454817783584cd312e2600802a13812028dfde
tomo <tomo14142356@gmail.com>
2022-10-28 05:16:56 +0900

    Add all runs update on project

--- projects/DeepLab/scripts/config_update.py
+++ projects/DeepLab/scripts/config_update.py
@@ -18,19 +18,21 @@ def get_epoch_from_path(model_path, all_epoch):
     return cur_epoch_str
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--id", type=str)
-    parser.add_argument("--entity", type=str, default="tomo")
-    parser.add_argument("--project", type=str, default="detectron2")
-    args = parser.parse_args()
+def update_one_run(run):
+    config = run.config
+    pretrain_config = config.get("pretrin", None)
+    args_config = config.get("args", None)
+    all_epoch = 1000
+    if pretrain_config is not None:
+        all_epoch = pretrain_config.get("epochs", 1000)
 
-    api = wandb.Api()
-    run = api.run(f"{args.entity}/{args.project}/{args.id}")
-
-    all_epoch = run.config["pretrin"]["epochs"]
-    model_path = run.config["args"]["model_path"]
     print(f"{all_epoch=}")
+
+    if args_config is None:
+        return
+    model_path = args_config.get("model_path", None)
+    if model_path is None or model_path == "":
+        return
     print(f"{model_path=}")
     epoch_str = get_epoch_from_path(model_path, all_epoch)
 
@@ -38,3 +40,23 @@ if __name__ == "__main__":
     run.config[key_name] = epoch_str
     print("update: ", f"{key_name=}, {epoch_str=}")
     run.update()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--id", type=str, default=None)
+    parser.add_argument("--entity", type=str, default="tomo")
+    parser.add_argument("--project", type=str, default="detectron2")
+    args = parser.parse_args()
+
+    api = wandb.Api()
+    if args.id is None:
+        runs = api.runs(args.project)
+    else:
+        run = api.run(f"{args.entity}/{args.project}/{args.id}")
+        runs = [run]
+    print("run num:", len(runs))
+
+    for run in runs:
+        print("start:", run)
+        update_one_run(run)
